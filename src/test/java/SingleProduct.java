3640535f2ae6ce7e3239a62639b73fe92fb02ba4
Antanosyan <antanosyan1987@gmail.com>
2025-03-27 02:26:25 +0400

    test are done

---
+++ src/test/java/SingleProduct.java
@@ -0,0 +1,9 @@
+public class SingleProduct {
+
+    public SingleProduct(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String name;
+    public String price;
+}
